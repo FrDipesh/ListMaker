fbfc90205ca531a34c8e9097f12114e64a7a50a8
618989 <poudel.618989@swl.k12.oh.us>
2025-12-01 08:55:23 -0500

    Done with List

--- src/Lab11.java
+++ src/Lab11.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lab11 {
+    Scanner scan = new Scanner(System.in);
+    static ArrayList<String> listMaker = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        listMaker.add("Husky");
+        listMaker.add("Tasty Sweets");
+        listMaker.add("Game");
+        listMaker.add("The Code");
+        listMaker.add("Rose");
+        listMaker.add("TV Shows");
+        listMaker.add("Creamy Mac and Cheese");
+        listMaker.add("Top School");
+        listMaker.add("Type Stuff");
+        listMaker.add("Not Home");
+    }
 }
