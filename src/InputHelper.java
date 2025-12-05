634ad0ed4ed9a3f1bde6acdc65e9616bf672a857
618989 <poudel.618989@swl.k12.oh.us>
2025-12-05 08:39:30 -0500

    Done with List

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,12 +1,135 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class InputHelper {
-    static Scanner scan = new Scanner(System.in);
-    static ArrayList<String> listMaker = new ArrayList<String>();
 
+    //getNonZeroLenString method returns a String a non-zero length back to user
+    public static String getNonZeroLenString(Scanner scan, String prompt) {
+        boolean valid = false;
+        String userInput;
 
-    // [Y/N] question
+        System.out.println(prompt);
+        do {
+            userInput = scan.nextLine();
+            if (userInput.length() > 0) {
+                valid = true;
+            }
+            else{
+                System.out.println("Error - must enter string of non-zero length");
+            }
+        } while (!valid);
+        return userInput;
+    }
+
+    //The getInt method returns a valid int value back to the user
+    public static int getInt(Scanner scan,  String prompt) {
+        boolean valid = false;
+        int userInput = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                userInput = scan.nextInt();
+                valid = true;
+            }
+            else{
+                System.out.println("Error - must enter integer value");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
+    }
+
+    //The getRangedInt method returns a valid int value back to the user between min and max
+    public static int getRangedInt(Scanner scan, String prompt, int min, int max) {
+        boolean valid = false;
+        int userInput = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                userInput = scan.nextInt();
+                if (userInput >= min && userInput <= max) {
+                    valid = true;
+                }
+                else{
+                    System.out.println("Error - input must be between " + min + " and " + max + ".");
+                    scan.nextLine();
+                }
+            }
+            else{
+                System.out.println("Error - must enter integer value");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
+    }
+
+    //The getDouble method returns a valid double value back to the user
+    public static double getDouble(Scanner scan, String prompt) {
+        boolean valid = false;
+        double userInput = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                userInput = scan.nextDouble();
+                valid = true;
+            }
+            else{
+                System.out.println("Error - must enter integer value");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
+    }
+
+    //The getRangedDouble method returns a valid int value back to the user between min and max
+    public static double getRangedDouble(Scanner scan, String prompt, double min, double max) {
+        boolean valid = false;
+        double userInput = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                userInput = scan.nextDouble();
+                if (userInput >= min && userInput <= max) {
+                    valid = true;
+                }
+                else{
+                    System.out.println("Error - input must be between " + min + " and " + max + ".");
+                    scan.nextLine();
+                }
+            }
+            else{
+                System.out.println("Error - must enter integer value");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
+    }
+
+    //The a value greater than 0
+    public static double getPositiveNonZeroInt(Scanner scan, String prompt) {
+        boolean valid = false;
+        int userInput = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                userInput = scan.nextInt();
+                if (userInput > 0) {
+                    valid = true;
+                }
+            }
+            else{
+                System.out.println("Error - must enter integer value greater than 0");
+                scan.nextLine();
+            }
+        } while (!valid);
+        return userInput;
+    }
+
     public static String getYNConfirm(Scanner scan, String prompt) {
         boolean validYN = false;
         String question= "";
@@ -26,24 +149,6 @@ public class InputHelper {
         } while (!validYN);
         return question;
     }
-
-    public static String addMethod(Scanner scan, String prompt){
-        boolean validAdd = false;
-        String yes = "";
-        String addToList;
-
-        System.out.println(prompt);
-        do {
-            if (scan.hasNextLine()) {
-                addToList = scan.nextLine();
-                if (yes.equalsIgnoreCase("y")) {
-                    listMaker.add(addToList);
-                    validAdd = true;
-                }
-                System.out.println("Enter a valid input: ");
-            }
-        } while (!validAdd);
-        return yes;
-    }
-
 }
+
+
--- src/Lab11.java
+++ src/Lab11.java
@@ -20,4 +20,7 @@ public class Lab11 {
         System.out.println();
     }
 
+
+
+
 }
